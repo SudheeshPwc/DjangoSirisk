194e80acf7d18cfa39c7bbddcbb051a6f26cf1c0
PWC <sudheesh.reddy.satti@in.pwc.com>
2019-04-10 16:50:29 +0530

    Sirisk Api

--- SIRISK/SIRISK/settings.py
+++ SIRISK/SIRISK/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Request',
+    'request',
     'rest_framework',
     'inspectdb_refactor',
 ]
